7d058b4643629ece942998fe85f5b0c0349d8660
ChrisAbra <chrisabra@live.com>
2023-03-20 11:53:20 +0000

    working first state with actions and reducers and subscriptions

--- addon/redux/src/Example/ExampleStore.cs
+++ addon/redux/src/Example/ExampleStore.cs
@@ -1,16 +1,12 @@
 namespace Redux.Example;
 
 
-public abstract record ExampleStoreAction : StoreAction {
-    public abstract ExampleState Reducer(ExampleState state);
-}
-
 public partial class ExampleStore : Store
 {
     private const string path = "/root/AppStateStore";
     public static ExampleStore Instance {get;private set;}
     public static ExampleState GetCurrentState()
-    {
+    { 
         return (ExampleState)Instance.CurrentState;
     }
 
